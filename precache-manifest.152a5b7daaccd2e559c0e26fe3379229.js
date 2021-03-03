dfb680a8035e0ce1513bebda1a0aa8160c1f2b05
sokolovskaya-evelina <sokolovskayaevi@gmail.com>
2021-03-03 09:11:20 +0300

    Updates

---
+++ precache-manifest.152a5b7daaccd2e559c0e26fe3379229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a42d4a4773a219d3385a98b52a1ba2",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "3eacd3ddc104f8ebadde",
+    "url": "/todolist/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "ae78b2068a81b2768a48",
+    "url": "/todolist/static/js/2.dbe590cb.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/todolist/static/js/2.dbe590cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eacd3ddc104f8ebadde",
+    "url": "/todolist/static/js/main.b939743a.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
